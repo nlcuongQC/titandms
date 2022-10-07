aa7a14092243e0b0c5eea11b5bfdc3e3ccbba15c
nlcuongQC <lecuong.nguyen@nfq.asia>
2022-10-08 03:52:11 +0700

    add report

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,9 +4,12 @@ export default defineConfig({
   e2e: {
     chromeWebSecurity: false,
     specPattern: "cypress/e2e/**/*Tests.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
+    },
     baseUrl: "https://www.saucedemo.com/",
     experimentalSessionAndOrigin: true,
-    supportFile: 'cypress/support/e2e.ts'
+    supportFile: 'cypress/support/e2e.ts',
   },
   defaultCommandTimeout: 15000,
   pageLoadTimeout: 60000,
@@ -15,5 +18,13 @@ export default defineConfig({
     openMode: 1
   },
   video: true,
-  screenshotOnRunFailure: true
+  screenshotOnRunFailure: true,
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
 });
